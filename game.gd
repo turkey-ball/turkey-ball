4ece7192cb3223b196ed163f6bbdad19d41ba19a
PyDario <dario@pichowsky.de>
2024-01-26 22:37:11 +0100

    move player controls to game

---
+++ game.gd
@@ -0,0 +1,32 @@
+extends Node
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(_delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_horizontal = Input.get_axis("ui_left", "ui_right")
+	var direction_vertical = Input.get_axis("ui_up", "ui_down")
+	if direction_horizontal:
+		$Arena/Player.velocity.x = direction_horizontal * SPEED
+	else:
+		$Arena/Player.velocity.x = 0
+	if direction_vertical:
+		$Arena/Player.velocity.y = direction_vertical * SPEED
+	else:
+		$Arena/Player.velocity.y = 0
+
+	$Arena/Player.move_and_slide()
--- player.gd
+++ player.gd
@@ -1,29 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _init():
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 
-func _physics_process(_delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction_horizontal = Input.get_axis("ui_left", "ui_right")
-	var direction_vertical = Input.get_axis("ui_up", "ui_down")
-	print(direction_horizontal)
-	print(direction_vertical)
-	if direction_horizontal:
-		velocity.x = direction_horizontal * SPEED
-	else:
-		velocity.x = 0
-	if direction_vertical:
-		velocity.y = direction_vertical * SPEED
-	else:
-		velocity.y = 0
-
-	move_and_slide()
