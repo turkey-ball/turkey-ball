8ccea3a7ec90cd50c91a5f572a6deb890b779a76
Lukas Kurth <github@lukas.kurth.rocks>
2024-01-26 23:11:37 +0100

    Player in main tree

--- player.gd
+++ player.gd
@@ -7,3 +7,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _init():
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 
+# Von: Lukas
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
+# Beispiel ist in 3D. Welche Cam in 2D. Dazu noche exit game qenn input 'quit
+# und physics process when multiplayer authority ...
+# https://www.youtube.com/watch?v=M0LJ9EsS_Ak
+#func _ready():
+#	cam.current = is_multiplayer_authority()
