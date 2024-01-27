bcd348a5bc39cf0d844b463f0804e7fa38ab2112
Steffen Wehner <ws@Schreibmaschine>
2024-01-27 17:26:28 +0100

    [clown] add animatin

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,6 +12,17 @@ func _init():
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 
+func _process(delta):
+	if velocity[0] > 0 :
+		$animation.flip_h = false 
+	if velocity[0] < 0 :
+		$animation.flip_h =  true
+	
+	if abs(velocity[0]) > 0.1 || abs(velocity[1]) > 0.1 :
+		$animation.play("walk")
+	else :
+		$animation.play("laugh")
+
 # Beispiel ist in 3D. Welche Cam in 2D. Dazu noche exit game qenn input 'quit
 # und physics process when multiplayer authority ...
 # https://www.youtube.com/watch?v=M0LJ9EsS_Ak
@@ -22,6 +33,7 @@ signal scare_turkey(pos: Vector2)
 signal kick_turkey(pos: Vector2)
 func _on_surrounding_child_entered_tree(node):
 	if node.name == "Turkey":
+		$animation.play("kick")
 		scare_turkey.emit(position)
 
 func _on_feet_child_entered_tree(node):
