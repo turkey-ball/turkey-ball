9276baf8a0677096b9e9b1fdadb67811dd5a4c7d
IreneSmober <irenesmober@gmail.com>
2024-01-27 22:18:59 +0100

    new collisionbodys

--- Scripts/turkey.gd
+++ Scripts/turkey.gd
@@ -34,8 +34,19 @@ func _process(_delta):
 	if abs(linear_velocity[0]) < slow and abs(linear_velocity[1]) < slow:
 		moving = false
 
+<<<<<<< Updated upstream
 	if $animation.animation == "explosion":
 		return
+=======
+	if (rotation_degrees > -3 and rotation_degrees < 3 and !moving):
+		#print ("change status")
+		#print ("aktueller status: ", status)
+		#rotation = 0
+		#linear_velocity = Vector2(0,0)
+		#status = 0
+		pass
+
+>>>>>>> Stashed changes
 	if status == 0:
 		$animation.play("idle")
 		idle_time += 1
@@ -61,6 +72,10 @@ func explode():
 # IT'S A FEATURE
 @export var haveChaosMode = false
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 func _on_area_2d_body_entered(body:Node2D):
 	print(body)
 	if body.name == "Player" \
@@ -78,6 +93,7 @@ func _on_area_2d_body_entered(body:Node2D):
 
 func _on_animation_animation_finished():
 	if $animation.animation == "explosion":
+<<<<<<< Updated upstream
 		position = turkeyStartPosition
 		$animation.play("idle")
 		status = 0
@@ -98,7 +114,28 @@ func _on_player_kick_turkey(pos):
 
 
 func _on_player_scare_collision_child_entered_tree(_node):
+=======
+		queue_free()
+
+
+func _on_player_scare_collision_child_entered_tree(node):
+	print(node)
+>>>>>>> Stashed changes
 	print("scared")
 	status = 2
 	rnd_nmb1 = rnd.randf_range(-1.0, 1.0)*0.1
 	rnd_nmb2 = rnd.randf_range(-1.0, 1.0)*0.1
+<<<<<<< Updated upstream
+=======
+
+
+func _on_player_interaction_collision_child_entered_tree(node):
+	if node.name == "Player":
+		linear_velocity = Vector2(0,0)
+		var new_direction = position - node.position
+		linear_velocity = new_direction
+		apply_impulse(new_direction * 3)
+		inertia = TURKEY_WEIGHT		
+		$scream1.play()	
+		status = 3
+>>>>>>> Stashed changes
