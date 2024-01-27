2cc12a15da8af1c221a1cd635919c7c179f1cf84
Steffen Wehner <ws@Schreibmaschine>
2024-01-27 18:15:10 +0100

    [player] fix idle animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,13 +6,23 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _init():
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
-	#$animation.play("walk")
-
+	
+func _ready():
+	$animation.play("laugh")
 # Von: Lukas
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 
+func _on_animation_animation_finished():
+	if $animation.animation == "kick":
+		$animation.pause()
+
+
 func _process(delta):
+	
+	if $animation.animation == "kick" :
+		print($animation.animation)
+		return
 	if velocity[0] > 0 :
 		$animation.flip_h = false 
 	if velocity[0] < 0 :
@@ -21,7 +31,7 @@ func _process(delta):
 	if abs(velocity[0]) > 0.1 || abs(velocity[1]) > 0.1 :
 		$animation.play("walk")
 	else :
-		$animation.play("laugh")
+		$animation.pause()
 
 # Beispiel ist in 3D. Welche Cam in 2D. Dazu noche exit game qenn input 'quit
 # und physics process when multiplayer authority ...
@@ -33,7 +43,7 @@ signal scare_turkey(pos: Vector2)
 signal kick_turkey(pos: Vector2)
 func _on_surrounding_child_entered_tree(node):
 	if node.name == "Turkey":
-		$animation.play("kick")
+
 		scare_turkey.emit(position)
 
 func _on_feet_child_entered_tree(node):
