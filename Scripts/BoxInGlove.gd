4042e8ace18b1a22762d9ef4851d515d7b493184
PyDario <dario@pichowsky.de>
2024-01-28 12:43:48 +0100

    cage the glove

--- Scripts/BoxInGlove.gd
+++ Scripts/BoxInGlove.gd
@@ -3,8 +3,12 @@ extends Node2D
 
 func _ready():
 	$Spring.points[0] = $DampedSpringJoint2D.position
-	$Glove.apply_impulse(Vector2(0, -1000))
 
 
 func _process(_delta):
 	$Spring.points[1] = $Glove.position
+
+
+func _on_button_pressed():
+	$InitialCage.queue_free()
+	$Glove.apply_impulse(Vector2(0, -1000))
